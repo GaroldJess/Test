f436b298b9fe5b63d4bf3fe48b79ade35714b364
GaroldJess <jesse.jensen.1989@gmail.com>
2025-02-11 11:38:09 -0700

    Add files via upload

---
+++ testBackground.ps1
@@ -0,0 +1,3 @@
+﻿$imagePath = "C:\Wallpaper\come_follow_me.jpeg"
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d $imagePath /f
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
